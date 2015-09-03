5e0dd5d6b9ed95540cdc99c49cbb534f50560cb2
DvanKooten <DvanKooten@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-03 03:35:35 +0000

    tagging v1.2
    git-svn-id: https://plugins.svn.wordpress.org/mailchimp-top-bar/tags/1.2@1236875 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ src/Options.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace MailChimp\TopBar;
+
+class Options {
+
+	/**
+	 * @var string
+	 */
+	private $options_key = '';
+
+	/**
+	 * @var array Array of options, without inherited values
+	 */
+	public $options = array();
+
+	/**
+	 * @var array Array of options with inherited values
+	 */
+	public $inherited_options = array();
+
+	/**
+	 * @var array
+	 */
+	public $parent_options = array();
+
+	/**
+	 * Constructor
+	 * @param string $options_key
+	 */
+	public function __construct( $options_key ) {
+		$this->options_key = $options_key;
+		$this->options = $this->load_options();
+		$this->inherited_options = $this->load_inherited_options();
+	}
+
+	/**
+	 * Get an option value
+	 *
+	 * @param      $key
+	 * @param bool $inherit
+	 *
+	 * @return mixed
+	 */
+	public function get( $key, $inherit = true ) {
+		if( $inherit ) {
+			return $this->inherited_options[ $key ];
+		} else {
+			return $this->options[ $key ];
+		}
+	}
+
+	/**
+	 * @return array
+	 */
+	private function get_defaults() {
+		return array(
+			'list' => '',
+			'enabled' => 1,
+			'show_to_administrators' => 1,
+			'cookie_length' => 90,
+			'color_bar' => '#ffcc00',
+			'color_text' => '#222222',
+			'color_button' => '#222222',
+			'color_button_text' => '#ffffff',
+			'size' => 'medium',
+			'sticky' => 1,
+			'text_email_placeholder' => __( 'Your email address..', 'mailchimp-top-bar' ),
+			'text_bar' => __( 'Sign-up now - don\'t miss the fun!', 'mailchimp-top-bar' ),
+			'text_button' => __( 'Subscribe', 'mailchimp-top-bar' ),
+			'redirect' => '',
+			'position' => 'top',
+
+			// options which should inherit if empty
+			'double_optin' => '',
+			'send_welcome' => '',
+			'text_subscribed' => '',
+			'text_error' => '',
+			'text_invalid_email' => '',
+			'text_already_subscribed' => '',
+		);
+	}
+
+	/**
+	 * @return array
+	 */
+	private function load_options() {
+		$options = (array) get_option( $this->options_key, array() );
+		$options = array_merge( $this->get_defaults(), $options );
+		return $options;
+	}
+
+	/**
+	 * @return array
+	 */
+	private function load_inherited_options() {
+
+		$this->parent_options = mc4wp_get_options( 'form' );
+		$inherited_options = $this->options;
+
+		// specify keys which should inherit if empty
+		$inheritance_keys = array(
+			'redirect',
+			'double_optin',
+			'send_welcome',
+			'text_subscribed',
+			'text_error',
+			'text_invalid_email',
+			'text_already_subscribed',
+		);
+
+		// Use parent value if option value is empty
+		foreach( $inheritance_keys as $key ) {
+			if( $inherited_options[ $key ] === '' ) {
+				$inherited_options[ $key ] = $this->parent_options[ $key ];
+			}
+		}
+
+		return $inherited_options;
+	}
+
+
+
+}
